b63ecbabe2b73a77fd087da0ce509a69653ad4b5
Mingcai SHEN <archsh@gmail.com>
2019-07-16 17:00:03 +0800

    Updated.

--- utils.go
+++ utils.go
@@ -1,61 +1,65 @@
 package main
 
 import (
-    //"github.com/rwtodd/sed-go"
-    "github.com/rwtodd/Go.Sed/sed"
-    "strings"
-    "os"
+	"fmt"
+	//"github.com/rwtodd/sed-go"
+	"github.com/rwtodd/Go.Sed/sed"
+	"os"
+	"strings"
 )
 
 type StringRewriter interface {
-    RunString(string) string
+	RunString(string, int) string
 }
 
 type PathRewriter struct {
-    engine *sed.Engine
+	engine *sed.Engine
 }
 
-func (self *PathRewriter) RunString(input string) string {
-    if nil != self.engine {
-        s, e := self.engine.RunString(input)
-        if nil != e {
-            return input
-        } else {
-            return strings.TrimSpace(s)
-        }
-    } else {
-        return input
-    }
+func (self *PathRewriter) RunString(input string, n int) string {
+	if nil != self.engine {
+		s, e := self.engine.RunString(input)
+		if nil != e {
+			return input
+		} else {
+			return strings.TrimSpace(s)
+		}
+	} else {
+		return input
+	}
 }
 
 func NewPathRewriter(cmd string) (pr *PathRewriter) {
-    pr = new(PathRewriter)
-    if cmd == "" {
-        return pr
-    }
-    engine, err := sed.New(strings.NewReader(cmd))
-    if nil == err {
-        pr.engine = engine
-    }
-    return
+	pr = new(PathRewriter)
+	if cmd == "" {
+		return pr
+	}
+	engine, err := sed.New(strings.NewReader(cmd))
+	if nil == err {
+		pr.engine = engine
+	}
+	return
 }
 
 type SegmentRewriter struct {
+	format string
 }
 
-func (self *SegmentRewriter) RunString(input string) string {
-    return input
+func (self *SegmentRewriter) RunString(input string, n int) string {
+	return fmt.Sprintf(self.format, n)
+	//return input
 }
 
 func NewSegmentRewriter(cmd string) (sr *SegmentRewriter) {
-    sr = new(SegmentRewriter)
-    return
+	sr = new(SegmentRewriter)
+	sr.format = cmd
+	return
 }
 
 func exists(path string, size int64) bool {
-    s, err := os.Stat(path)
-    if nil != err || s.Size() < size {
-        return false
-    }
-    return true
+	s, err := os.Stat(path)
+	if nil != err || s.Size() < size {
+		return false
+	}
+	return true
 }
