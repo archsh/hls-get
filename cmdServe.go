1c8413bc9bfa83b7d6905bbc3aaa9687dda07361
Mingcai SHEN <archsh@gmail.com>
2019-07-20 00:26:31 +0800

    Updated.

--- cmdServe.go
+++ cmdServe.go
@@ -1,30 +1,68 @@
 package main
 
 import (
+	"crypto/md5"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
-	log "github.com/Sirupsen/logrus"
-	"github.com/gorilla/handlers"
-	"github.com/gorilla/mux"
-	"github.com/spf13/cobra"
 	"hls-get/htmldocs"
 	"io/ioutil"
 	"net/http"
 	"os"
+	"time"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+	"github.com/spf13/cobra"
 )
 
 var (
-	debug   bool
-	listen  string
-	root    string
-	combine bool
-	ffmpeg  string
-	timeout int
+	debug        bool
+	listen       string
+	root         string
+	combine      bool
+	ffmpeg       string
+	timeout      int
+	concurrent   int
+	downloadChan chan *downloadReq
 )
 
 type downloadReq struct {
+	id   string
 	Name string `json:"name"`
 	Url  string `json:"url"`
+}
+
+type downloadResp struct {
+	Code    int    `json:"code"`
+	Id      string `json:"id"`
+	Status  int    `json:"status"`
+	Message string `json:"msg"`
+}
+
+func writeResponse(w http.ResponseWriter, code int, id string, status int, message string) {
+	var resp = downloadResp{
+		Code:    code,
+		Id:      id,
+		Status:  status,
+		Message: message,
+	}
+	if bs, e := json.Marshal(resp); nil != e {
+		w.WriteHeader(500)
+		w.Header().Set("Content-Type", "plain/text")
+		_, _ = w.Write([]byte("OK"))
+	} else {
+		w.WriteHeader(200)
+		w.Header().Set("Content-Type", "application/json")
+		_, _ = w.Write(bs)
+	}
+}
+
+func md5sum(s string) string {
+	h := md5.New()
+	h.Write([]byte(s))
+	return hex.EncodeToString(h.Sum(nil))
 }
 
 func downloadHandler(w http.ResponseWriter, r *http.Request) {
@@ -44,9 +82,26 @@ func downloadHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	} else {
 		log.Debugln("downloadHandler:>", req.Name, req.Url)
-		w.WriteHeader(200)
-		w.Header().Set("Content-Type", "plain/text")
-		_, _ = w.Write([]byte("OK"))
+		req.id = md5sum(req.Url)
+		if len(downloadChan) >= concurrent {
+			writeResponse(w, -1, "", -1, "Tasks full, please wait...")
+		} else {
+			downloadChan <- &req
+			writeResponse(w, 0, req.id, 0, "")
+		}
+
+	}
+}
+
+func downloadTask() {
+	log.Infoln("downloadTask:> started")
+	for t := range downloadChan {
+		log.Debugln("downloadTask:>", t.id, t.Name, t.Url)
+		go func() {
+			log.Debugln(">>>> Downloading:", t.id, t.Url)
+			time.Sleep(30 * time.Second)
+			log.Debugln(">>>> Downloaded:", t.id, t.Url)
+		}()
 	}
 }
 
@@ -65,6 +120,11 @@ var serveCmd = &cobra.Command{
 		router.PathPrefix("/").Handler(
 			handlers.CombinedLoggingHandler(os.Stdout, http.StripPrefix("/", http.FileServer(htmldocs.Assets))))
 		log.Infoln("Start to serve @", listen, "...")
+		if concurrent < 1 {
+			concurrent = 5
+		}
+		downloadChan = make(chan *downloadReq, concurrent)
+		go downloadTask()
 		if e := http.ListenAndServe(listen, handlers.CombinedLoggingHandler(os.Stdout, router)); nil != e {
 			fmt.Println("Serve failed:>", e)
 		}
@@ -78,4 +138,5 @@ func init() {
 	serveCmd.Flags().StringVar(&ffmpeg, "ffmpeg", "ffmpeg", "FFMPEG executable path")
 	serveCmd.Flags().BoolVar(&combine, "combine", false, "Combine segments into MP4 file")
 	serveCmd.Flags().IntVar(&timeout, "timeout", 20, "Request timeout in seconds.")
+	serveCmd.Flags().IntVar(&concurrent, "concurrent", 5, "Concurrent download tasks")
 }
